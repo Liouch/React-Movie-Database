b06e60c578cf31607961c9e6cf2b6b232c3e64f7
Liou <liou.wang.it@gmail.com>
2021-12-06 09:40:57 +0100

    Create Header component and the styles it's going to use (responsive styles included)

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Header from "./components/Header/Header";
 import { GlobalStyle } from "./GlobalStyles";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       Start here
       <GlobalStyle />
     </div>
