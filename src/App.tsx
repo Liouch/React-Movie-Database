31055b5ebae181312a2517d3f2c51c1c8dca2584
Liou <liou.wang.it@gmail.com>
2021-12-06 09:22:50 +0100

    Change file extension to .tsx and create global styles to set the styles of the application

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { GlobalStyle } from "./GlobalStyles";
+
+function App() {
+  return (
+    <div className="App">
+      Start here
+      <GlobalStyle />
+    </div>
+  );
+}
+
+export default App;
